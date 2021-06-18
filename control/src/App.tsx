6aed2444af0a8d79ca207783db16aacaadfcaac8
Dawson Toth <dawson@toth.support>
2021-06-18 14:10:06 -0400

    feat: Stub reset button

--- control/src/App.tsx
+++ control/src/App.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { Clock } from './components/clock';
-import { Timeline } from './components/timeline';
+import React from "react";
+import { Clock } from "./components/clock";
+import { Controls } from "./components/controls";
+import { Timeline } from "./components/timeline";
 
 export function App() {
-	return (
-		<div className="App">
-			<Clock />
-			<Timeline />
-		</div>
-	);
+  return (
+    <div className="App">
+      <Clock />
+      <Controls />
+      <Timeline />
+    </div>
+  );
 }
 
 // Clock
---
+++ control/src/components/controls.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./controls.scss";
+
+export function Controls() {
+  return (
+    <div className="controls">
+      <button>Reset</button>
+    </div>
+  );
+}
